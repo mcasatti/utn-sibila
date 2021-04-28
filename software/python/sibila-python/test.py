aacf18188a8b2be070e3b369885cb7b4b23d92c0
Martin Casatti <mcasatti@frc.utn.edu.ar>
2021-04-27 21:38:19 -0300

    Actualizados archivos de testing
    Se actualizaron algunos archivos utilizados para testing y el archivo de instalacion de paquetes de Python (requirements.txt)

--- software/python/sibila-python/test.py
+++ software/python/sibila-python/test.py
@@ -5,19 +5,25 @@ from managers.knowledgemanager import KnowledgeManager
 from managers.textmanager import TextManager
 from entities.graphclasses import *
 from rich import print,inspect
+from rich.console import Console
 
 
 def main():
+    console = Console()
+    console.clear()
+
     FORMAT = "%(message)s"
     #logging.basicConfig(level=logging.INFO,handlers=[RichHandler()])
     logging.basicConfig(
-        level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+        level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
     )
     log = logging.getLogger("rich")
-    km = KnowledgeManager()
+    #km = KnowledgeManager()
     tm = TextManager()
-
+    km = KnowledgeManager()
+    
     try:
+        '''
         correcciones, error = tm.checkOrtografia("Prueva de tecto. Veamos que pasa con palavras divididas por un punto.O sin punto")
         #inspect(correcciones,title="TITULO",sort=False,methods=False)
         if error:
@@ -27,21 +33,22 @@ def main():
         else:
             print ("Todo correcto")
         '''
-        c1 = Concepto(Nombre="ConceptoExtraño")
-        result, id = km.insConcepto(c1)
-        print (result,id)
-        conceptos = ['MODELO','DISEÑO','PROGRAMA','OBJETO']
-
-        result = km.getPath(conceptoInicial=c1,conceptosIncluidos=conceptos)
-        print (result)
-
-        result = km.getPathsFrom(conceptoInicial=c1,profundidad=1)
-        print (result)
         
-        result = cm.getPathsByType(relacion=Relacion('EsUn'))
-        print (result)
+        
+        tm.addWordsToDict(['POO','PPR','OOP','member'])
+        texto = "Los objetos son parte del paradigma POO y están formados por métodos y atributos y se comunican por medio de mensajes"
+        #print (tm.check(texto=texto))
         '''
-        # inspect(query)
+        print (tm.tokenize(texto))
+        '''
+        #words = ['POO','EsUn','PARADIGMA','TEST','MARTIN','PRUEBA','TieneUn','Referencia','Utiliza']
+        #print(km.setClass(word=words[0]))
+        #print(km.setClasses (words=words))
+        correcciones,tokens = km.tokenizeRespuesta(texto=texto,textManager=tm,includeStopWords=True)
+        print (correcciones,tokens)
+        tkn_str = [ (tk['text'],tk['lema'],tk['claseDB'],tk['claseSugerida']) for tk in tokens ]
+        print (tkn_str)
+
     except Exception as e:
         inspect (e)
         log.exception(e)
